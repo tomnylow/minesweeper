2d91d8cb4d27b3999b59ec1834f49bb9127fd6e8
vniuser <vni@user>
2025-10-14 10:07:54 +0300

    random mines positions
    (cherry picked from commit 744f90dd06361ce39374ca8c27a20d4650196c89)

--- src/main/kotlin/minesweeper/minesweeper.kt
+++ src/main/kotlin/minesweeper/minesweeper.kt
@@ -1,5 +1,27 @@
 package minesweeper
 
-fun main() {
+import kotlin.random.Random
 
+fun main() {
+    println("How many mines do you want on the field?")
+    val mineCount = readLine()!!.toInt()
+    val size = 9
+    val totalCells = size * size
+
+    if (mineCount !in 0..totalCells) throw IllegalStateException("Invalid number of mines")
+
+    val mines = mutableSetOf<Pair<Int, Int>>()
+    while (mines.size < mineCount) {
+        val row = Random.nextInt(size)
+        val col = Random.nextInt(size)
+        mines.add(Pair(row, col))
+    }
+
+    for (i in 0 until size) {
+        val row = StringBuilder()
+        for (j in 0 until size) {
+            row.append(if (mines.contains(i to j)) 'X' else '.')
+        }
+        println(row.toString())
+    }
 }
